58268ba9bc1e8b5a1d74af20dd63ecfbd70f9f97
talesa <adam.gol1993@gmail.com>
2015-05-04 04:33:12 -0700

    commit

--- pcb/js/scripts.js
+++ pcb/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var images = ['01.png', '02.png', '03.png', '04.png', '05.png'];
+    var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 
     $('.full').css({'background-image': 'url(./img/backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
