085d093bc7fa503c303748eb8fe4d05b0388c6ce
talesa <adam.gol1993@gmail.com>
2022-12-06 20:50:32 +0100

    update

--- pcb/js/scripts.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
-
-    $('.full').css({'background-image': 'url(./img/backgrounds/' + images[Math.floor(Math.random() * images.length)] + ')'});
-});
